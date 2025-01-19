440d5632fa7a3b20f1e134fab149baaa48bdd3f6
Leivo Sepp <leivo@robootika.com>
2025-01-19 22:43:04 +0200

    Script improvement.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Shelly Internet Check
 
-This Shelly script monitors the internet connectivity and performs an off-on cycle on the relay if the connection is lost for a specified number of checks.
+This Shelly script monitors the internet connectivity and performs an off-on cycle on the relay if there is no internet connection for a specified number of checks.
 
 The main purpose of this script is to connect your internet router through the Shelly device and automatically reboot the router if the internet connection is down. 
 
@@ -14,15 +14,15 @@ The main purpose of this script is to connect your internet router through the S
 
 1. Copy the contents of `ShellyInternetCheck.js` to your Shelly device's script editor.
 2. Configure the following settings:
-   - `checkInterval`: Choose the interval beween checking the Shelly time, default 10 minutes.
-   - `cyclesBeforeAction`: Set the number of cycles to check before Shelly relay performs off-on cycle.
-
-## License
-
-This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+   - `numberOfFails`: Set the number of cycles to check before Shelly relay performs off-on cycle
+   - `pingTime`: Choose the interval beween checking the Shelly time, default 10 minutes.
+   - `relayId`: Shelly relayID to control the external device power supply
 
 ## Author
 
-Created by Leivo Sepp, 10.01.2025
+Modified by Leivo Sepp, 10.01.2025
 
 [GitHub Repository](https://github.com/LeivoSepp/Shelly-Internet-Check)
+
+Inspired by this project: [ALLTERCO/shelly-script-examples](https://github.com/ALLTERCO/shelly-script-examples/blob/main/router-watchdog.js)
+
--- ShellyInternetCheck.js
+++ ShellyInternetCheck.js
@@ -1,60 +1,91 @@
 /*
-Created by Leivo Sepp, 2025
-Licensed under the MIT License
+Modified by Leivo Sepp, 2025
+Inspired by this script: https://github.com/ALLTERCO/shelly-script-examples/blob/main/router-watchdog.js 
 https://github.com/LeivoSepp/Shelly-Internet-Check
 
 This Shelly script monitors the internet connectivity 
-and performs an off-on cycle on the relay if the connection is lost for a specified number of checks.
+and performs an off-on cycle on the relay if there is no connection for a specified number of checks.
 
 The main purpose of this script is to connect your internet router through the Shelly device 
 and automatically reboot the router if the internet connection is down. 
 */
 
-const checkInterval = 10; //10 minutes between checks
-const cyclesBeforeAction = 6; //6 x 10 = 1h to perform Shelly relay off-on
-const relayId = 0; //Relay ID to control the external device power supply
+const CONFIG = {
+    endpoints: [
+        "https://global.gcping.com/ping",
+        "https://us-central1-5tkroniexa-uc.a.run.app/ping",
+    ],
+    //number of failures that trigger the reset
+    numberOfFails: 3,
+    //time in seconds to retry a "ping"
+    pingTime: 300, // 300s = 5mins 
+    relayId: 0, //Relay ID to control the external device power supply
+
+    //time in seconds after which the http request is considered failed
+    httpTimeout: 10,
+    //time in seconds for the relay to be off
+    toggleTime: 5,
+};
+
+let endpointIdx = 0;
+let failCounter = 0;
+let pingTimer = null;
 
 const scriptId = Shelly.getCurrentScriptId();
-let isShellyTimeOk = false;
-let counter = 0;
-/* set the script to sart automatically on boot */
+
 function setAutoStart() {
     if (!Shelly.getComponentConfig("script", scriptId).enable) {
-        Shelly.call('Script.SetConfig', { id: scriptId, config: { enable: true } },
-            function (res, err, msg, data) {
-                if (err != 0) {
-                    console.log("Internet check autostart is not enabled.", msg, ". After Shelly restart, this script will not check internet connectivity.");
-                }
-            });
+        Shelly.call('Script.SetConfig', { id: scriptId, config: { enable: true } });
     }
 }
 
-//check Shelly time
-function checkShellyTime() {
-    const shEpochUtc = Shelly.getComponentStatus("sys").unixtime;
-    print("Shelly", new Date(shEpochUtc * 1000));
-    if (shEpochUtc > 0) {
-        isShellyTimeOk = true;
-        counter = 0;
-    } else {
-        counter++;
-        print("counter:", counter);
-        if (counter >= cyclesBeforeAction) {
-            counter = 0;
-            actionAfterCycles();
+function restartRelay() {
+    // Toggle Shelly relay
+    Shelly.call("Switch.Set", { id: CONFIG.relayId, on: false, toggle_after: CONFIG.toggleTime });
+}
+
+function pingEndpoints() {
+    Shelly.call(
+        "http.get",
+        { url: CONFIG.endpoints[endpointIdx], timeout: CONFIG.httpTimeout },
+        function (response, error_code, error_message) {
+            //http timeout, magic number, not yet documented
+            if (error_code === -114 || error_code === -104) {
+                print("Failed to fetch ", CONFIG.endpoints[endpointIdx]);
+                failCounter++;
+                print("Rotating through endpoints");
+                endpointIdx++;
+                endpointIdx = endpointIdx % CONFIG.endpoints.length;
+            } else {
+                failCounter = 0;
+                print("Ping success");
+            }
+            if (failCounter >= CONFIG.numberOfFails) {
+                print("Too many fails, resetting...");
+                failCounter = 0;
+                Timer.clear(pingTimer);
+                restartRelay();
+                return;
+            }
         }
-        return;
-    }
+    );
 }
 
-//action after checking Shelly time for a number of cycles
-function actionAfterCycles() {
-    // perform power off and on
-    Shelly.call("Switch.Set", { id: relayId, on: false, toggle_after: 2 });
-}
-
-//execute the checkShellyTime when the script starts
+print("Start watchdog timer");
 setAutoStart();
-checkShellyTime();
-//start loop-timer to check Shelly time 
-Timer.set(1000 * 60 * checkInterval, true, checkShellyTime);
+pingTimer = Timer.set(CONFIG.pingTime * 1000, true, pingEndpoints);
+
+Shelly.addStatusHandler(function (status) {
+    //is the component a switch
+    if (status.name !== "switch") return;
+    //is it the one with id 0
+    if (status.id !== CONFIG.relayId) return;
+    //does it have a delta.source property
+    if (typeof status.delta.source === "undefined") return;
+    //is the source a timer
+    if (status.delta.source !== "timer") return;
+    //is it turned on
+    if (status.delta.output !== true) return;
+    //start the loop to ping the endpoints again
+    pingTimer = Timer.set(CONFIG.pingTime * 1000, true, pingEndpoints);
+});
